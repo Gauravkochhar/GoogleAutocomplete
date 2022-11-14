0f1b62ffde781a1ef775f151eafb65ec24bcc6f7
Gaurav Kochhar <Gauravkochhar8822@gmail.com>
2022-11-14 23:54:59 +0530

    Google Autocomplete input done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'google-address-autocomplete';
+  autocompleteInput: string | undefined;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  getAddressOnChange(place: Object) {
+    console.log('selected address:', place);
+  }
+
+  ngOnDestroy() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { GoogleplaceDirective } from './directive/google-place.directive';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleplaceDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
